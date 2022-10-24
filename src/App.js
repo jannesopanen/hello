88544b74c0a670513a344d26edba9deb53b6fcc3
= <=>
2022-10-25 02:40:30 +0300

    First Setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <center><a href = "pillu.net"><u>Like Links?</u></a></center>
+    </div>
+  );
+}
+
+export default App;
